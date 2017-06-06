e059ab12a3ed442a1d96961663a5bcf4880b1531
Stephen Wicks <stevewicks1@gmail.com>
2017-06-06 16:35:38 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,81 @@
+// 1.
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// A:
+
+
+function max(firstNum,secondNum) {
+
+    if (firstNum > secondNum) {
+    return(firstNum);
+     } else {
+        return(secondNum);
+    }
+} 
+    max(5, 10);
+    
+
+
+
+// 2.
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// A:
+
+function maxOfThree(num1,num2,num3) { 
+
+    var max = Math.max(num1, num2, num3);
+    return max;
+}
+max0fThree(5,10,15);
+
+// 3.
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// A:
+
+function isVowel(char){
+    // Your answer here
+}
+
+
+// 4.
+// Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
+// A:
+
+
+
+// 5.
+// Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
+// A:
+
+
+
+// 6.
+// Write a function called `getLength` that takes one parameter (a string) and returns the length
+// A:
+
+
+
+// 7.
+// Write a function called `greaterThan` that takes two parameters
+// and returns `true` if the second parameter is greater than the first.
+// Otherwise the function should return `false`.
+// A:
+
+
+
+// 8.
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+// A:
+
+
+
+// 9.
+// Write a function called `madlib` that takes 4 or more parameters (words).
+// The function should insert the words into a pre-defined sentence.
+// Finally the function should return that sentence.
+// Note: When I say words and sentence I mean strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+// A:
